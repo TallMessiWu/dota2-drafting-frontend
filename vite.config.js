6cb489fd4bf3ebda20d4d8cdf2ed8bd4419303fa
(Messi) Junlin Wu <tallmessiwu@qq.com>
2023-01-17 02:54:19 -0800

    :tada: Initial commit.

---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import vue from "@vitejs/plugin-vue"
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import {defineConfig} from "vite"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // 配置默认根路径
+  base: "./",
+  server: {
+    host: "0.0.0.0",
+    port: 666
+  },
+  plugins: [
+    // 配置响应式语法糖
+    vue({
+      reactivityTransform: true
+    }),
+    // 配置按需引入组件和样式
+    AutoImport({
+      resolvers: [
+        IconsResolver({
+          prefix: "Icon"
+        }),
+        ElementPlusResolver()
+      ],
+    }),
+    Components({
+      resolvers: [
+        IconsResolver({
+          enabledCollections: ["ep"]
+        }),
+        ElementPlusResolver()
+      ],
+    }),
+    Icons({
+      autoInstall: true
+    })
+  ]
+})
